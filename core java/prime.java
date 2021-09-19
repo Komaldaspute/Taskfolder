1fe75a8440b26467933a8c44c747f293a214ea17
Komaldaspute <90999485+Komaldaspute@users.noreply.github.com>
2021-09-20 01:34:30 +0530

    Add files via upload

---
+++ core java/prime.java
@@ -0,0 +1,19 @@
+class fact
+{
+public static void main(String args[])
+{
+  int num=Integer.parseInt(args[0])
+ f0r(i=2;i<num;i++)
+{
+if(n%i==0)
+{
+System.out.println("not prime");
+}
+}
+
+if(i==num)
+{
+System.out.println("prime number");
+}
+}
+}
